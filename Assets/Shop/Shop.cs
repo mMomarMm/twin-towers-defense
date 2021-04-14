8541cae07aa24dc838821a95e094e7f26636ad0c
mMomarMm <mMomarMm@users.noreply.github.com>
2021-04-14 14:17:50 -0500

    fixed more camera things

--- Assets/Shop/Shop.cs
+++ Assets/Shop/Shop.cs
@@ -7,33 +7,39 @@ public class Shop : MonoBehaviour
 {
     public static bool usando_blaster = true;
     public static bool torreta_comprada = false;
-    public bool wasBought;
+    public bool wasBoughtT, wasBoughtS = false;
+    //wasBoughtT = was bought turret, wasBoughtS = was bought shotting time;
     public GameObject Blaster, Turret;
     public Image TbuyButton, CbuyButton, BbuyButton;
     //TbuyButton = turret buy button, CbuyButton = clock buy button, BbuyButton = bomb buy button
 
     private void Start()
     {
-        wasBought = false;
-        TbuyButton.GetComponent<Image>().color = new Color32(91, 255, 32, 100);
+        TbuyButton.GetComponent<Image>().color = new Color32(91, 255, 32, 255);
     }
     public void BuyTurret()
     {
-        if (!torreta_comprada && Dollar.dollars >= 150)
+        if (!wasBoughtT && Dollar.dollars >= 150)
         {
+            wasBoughtT = true;
             Dollar.dollars -= 150;
             TbuyButton.GetComponent<Image>().color = new Color32(74, 184, 42, 255);
             Blaster.SetActive(false);
             Turret.SetActive(true);
         }
+        else
+        {
+            Blaster.SetActive(true);
+            Turret.SetActive(false);
+        }
     }   
     public void ShottingTime()
     {
-        if (Dollar.dollars >= 50 && !wasBought)
+        if (Dollar.dollars >= 50 && !wasBoughtS)
         {
             Weapon.startTimeBtwShots = 0.3f;
             turret_gun.startTimeBtwShots = 0.7f;                                                                         
-            wasBought = true;
+            wasBoughtS = true;
         }
     }
     public void Bomb()
