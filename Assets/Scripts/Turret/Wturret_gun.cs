6f04a0902fb556fc7895dc90745ffd0b114fc51f
mMomarMm <mMomarMm@users.noreply.github.com>
2021-04-15 15:23:52 -0500

    make flame thrower animations

---
+++ Assets/Scripts/Turret/Wturret_gun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wturret_gun : MonoBehaviour
+{
+    public float offset;
+    public GameObject projectile;
+    public Transform shotPoint;
+    public static bool turret_flip;
+    private SpriteRenderer spriteRender;
+    public CameraShake CameraShake;
+    private float timeBtwShots;
+    public static float startTimeBtwShots;
+
+    private void Start()
+    {
+        startTimeBtwShots = 0;
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        // Handles the weapon rotation
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+
+        if (rotZ < 89 && rotZ > -89)
+        {
+            spriteRender.flipY = false;
+            turret_flip = false;
+        }
+        else
+        {
+            spriteRender.flipY = true;
+            turret_flip = true;
+        }
+
+        if (timeBtwShots <= 0 && Input.GetMouseButton(0))
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+            StartCoroutine(CameraShake.Shake(.15f, .4f));
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
